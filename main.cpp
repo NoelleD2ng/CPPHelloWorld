6adf48c3005f2b400c3b2555ed3a103a9fb32ae6
NoelleD2ng <168130162+NoelleD2ng@users.noreply.github.com>
2025-05-01 21:13:36 -0700

    made main file

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by noell on 5/1/2025.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by noell on 5/1/2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
